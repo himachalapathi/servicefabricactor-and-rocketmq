04b29186ebfc22ea5f803cca363caf14aef90257
Murugan <velsan@bizruntime.com>
2018-11-27 21:10:29 +0530

    servicefabricactor

---
+++ rocketmq/src/main/java/Hemadri/rocketmq/App.java
@@ -0,0 +1,13 @@
+package Hemadri.rocketmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
